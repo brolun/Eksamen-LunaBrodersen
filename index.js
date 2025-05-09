1a0f26204b4276f8e065aa9664f1d0f1f3d2b501
Luna Brodersen <luna.brodersen@gmail.com>
2025-05-09 21:41:55 +0200

    Vise liste med brukere

--- index.js
+++ index.js
@@ -3,4 +3,34 @@ import { getUsers } from "./requests/GET.js";
 import {} from "./requests/PUT.js";
 import {} from "./requests/DELETE.js";
 
-getUsers();
+function createUserCard(user) {
+	const userCard = document.createElement("li");
+	userCard.classList.add("user-card");
+
+	userCard.innerHTML = `
+    <img src="${user.picture.large}" alt="Profilbilde">
+            <h3>${user.name.first} ${user.name.last}</h3>
+              <p>${user.dob.age} år</p>
+              <p>${user.location.city}, ${user.location.country}</p>`;
+
+	return userCard;
+}
+
+async function showUsers() {
+	const users = await getUsers();
+	if (!users || users.length === 0) {
+		console.log("Ingen brukere funnet.");
+		return;
+	}
+	console.log(users);
+
+	const userList = document.getElementById("user-list");
+	userList.innerHTML = "";
+
+	users.forEach((user) => {
+		const userCard = createUserCard(user);
+		userList.appendChild(userCard);
+	});
+}
+
+showUsers();
--- requests/GET.js
+++ requests/GET.js
@@ -3,10 +3,15 @@ import { crudUrl } from "./auth.js";
 async function getUsers() {
 	try {
 		const response = await fetch(crudUrl);
+		if (!response.ok) {
+			throw new Error("HTTP error! status:", response.status);
+		}
 		const data = await response.json();
 		console.log(data);
+		return data;
 	} catch (error) {
 		console.error("Klarte ikke å hente brukere", error);
+		return [];
 	}
 }
 
