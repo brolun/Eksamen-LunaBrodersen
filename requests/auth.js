96d5deb2f8c7a40a7dbcba4b4d22a0f1b7c4d7e7
Luna Brodersen <luna.brodersen@gmail.com>
2025-05-09 19:01:25 +0200

    Gjøre klar auth.js og index.html

---
+++ requests/auth.js
@@ -0,0 +1,5 @@
+const endpoint = "5f3b0c4e8a2d4c7b8e1f3a9b6c7d8e9f"; // Erstatt med ny nøkkel når den utløper
+const crudUrl = `https://crudcrud.com/api/${endpoint}/users`;
+const randomUserUrl = `https://randomuser.me/api/`;
+
+export { crudUrl, randomUserUrl };
