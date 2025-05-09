88ea254056abfe72808b984345e3a92366083b5d
Luna Brodersen <luna.brodersen@gmail.com>
2025-05-09 23:02:02 +0200

    Dele mellom match-forslag og favoritt-oversikt

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ import {} from "./requests/PUT.js";
 import {} from "./requests/DELETE.js";
 
 function createUserCard(user) {
-	const userCard = document.createElement("li");
+	const userCard = document.createElement("div");
 	userCard.classList.add("user-card");
-
 	userCard.innerHTML = `
         <img src="${user.picture.large}" alt="Profilbilde">
         <h3>${user.name.first} ${user.name.last}</h3>
@@ -14,25 +13,38 @@ function createUserCard(user) {
         <p>${user.location.city}, ${user.location.country}</p>
         <button>Nei</button>
         <button>Ja</button>`;
-
 	return userCard;
 }
 
-async function showUsers() {
+async function showPotentialMatch() {
 	const users = await getUsers();
 	if (!users || users.length === 0) {
-		console.log("Ingen brukere funnet.");
+		console.log("Ingen mulige match funnet.");
 		return;
 	}
 	console.log(users);
+	const randomIndex = Math.floor(Math.random() * users.length);
+	const randomUser = users[randomIndex];
+	const potentialMatch = document.getElementById("match-suggestion");
+	potentialMatch.innerHTML = "";
+	const userCard = createUserCard(randomUser);
+	potentialMatch.appendChild(userCard);
+}
 
-	const userList = document.getElementById("user-list");
-	userList.innerHTML = "";
-
+async function showFavorites() {
+	const users = await getUsers();
+	if (!users || users.length === 0) {
+		console.log("Ingen favoritter funnet.");
+		return;
+	}
+	console.log(users);
+	const favoriteList = document.getElementById("favorites");
+	favoriteList.innerHTML = "";
 	users.forEach((user) => {
 		const userCard = createUserCard(user);
-		userList.appendChild(userCard);
+		favoriteList.appendChild(userCard);
 	});
 }
 
-showUsers();
+showPotentialMatch();
+showFavorites();
